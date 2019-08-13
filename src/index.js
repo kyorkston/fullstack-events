c45d365832f7d16cc28daea0cfbc50a1f5d0dffb
Kristie Yorkston <kyorkston@gmail.com>
2019-08-13 16:18:28 +0100

    add apollo client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-var { ApolloServer } = require('apollo-server')
+var { ApolloServer, gql } = require('apollo-server')
+const Boost = require('apollo-boost')
+const ApolloClient = Boost.default
+const fetch = require('node-fetch')
 
 const models = require('../models')
 const typeDefs = require('../data/schema')
@@ -10,3 +13,14 @@ server.listen(4000).then(({url}) => {
   console.log('Running a GraphQL API server at localhost:4000/graphql')
 })
 
+const client = new ApolloClient({fetch: fetch})
+
+client.query({
+    query: gql`
+      allUsers{
+        name
+        email
+      }
+    `
+  })
+  .then(result => console.log(result));
